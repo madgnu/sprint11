e9fa95ae964659714de0b3ec8b5359bcc8fdc1be
Васильев Михаил <madgnu@madg.nu>
2020-03-10 02:56:20 +0300

    to review 2

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const initialCards = [
+        { name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg' },
+        { name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg' },
+        { name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg' },
+        { name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg' },
+        { name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg' },
+        { name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg' },
+        { name: 'Нургуш', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg' },
+        { name: 'Тулиновка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg' },
+        { name: 'Остров Желтухина', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg' },
+        { name: 'Владивосток', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg' }
+    ];
+
+const cardList = document.querySelector('.places-list');
+const popup = document.querySelector('.popup');
+const popupOpenBtn = document.querySelector('.user-info__button');
+const popupCloseBtn = document.querySelector('.popup__close');
+const newForm = document.forms.new;
+
+const createCard = ({name, link}) => {
+    const el = document.createElement('div');
+    const tpl = `
+        <div class="place-card__image" style="background-image: url(${link})">
+            <button class="place-card__delete-icon"></button>
+        </div>
+        <div class="place-card__description">
+            <h3 class="place-card__name">${name}</h3>
+            <button class="place-card__like-icon"></button>
+        </div>`;
+    el.classList = 'place-card';
+    el.insertAdjacentHTML('beforeend', tpl);
+    cardList.appendChild(el);
+}
+
+const openForm = () => popup.classList.add('popup_is-opened');
+const closeForm = () => popup.classList.remove('popup_is-opened') || newForm.reset();
+const escapeForm = (event) => (popup.classList.contains('popup_is-opened') && event.key === 'Escape') ? closeForm(): null;
+const likeChange = (event) => (event.target.classList.contains('place-card__like-icon')) ? event.target.classList.toggle('place-card__like-icon_liked') : null;
+const deleteCard = (event) => (event.target.classList.contains('place-card__delete-icon')) ? event.target.parentNode.parentNode.remove() : null;
+const submitForm = (event) => { event.preventDefault(); createCard({ name: newForm.elements.name.value, link: newForm.elements.link.value }); closeForm(); }
+
+cardList.addEventListener('click', likeChange);
+cardList.addEventListener('click', deleteCard);
+popupOpenBtn.addEventListener('click', openForm);
+popupCloseBtn.addEventListener('click', closeForm);
+newForm.addEventListener('submit', submitForm);
+document.addEventListener('keydown', escapeForm);
+
+initialCards.forEach(createCard);
+
+/*Надо исправить:
+* Применение Flux архитектуры для решения столь простой задачи это оверхед. Количество кода, подключеного для решения задачи многократно превышает количество кода, требующегося для решения.
+* Проект невозможно запустить без поднятия локального сервера из-за CORS и применения ES6 импортов.
+* Использовать модули можно будет после прохождения спринтов по архитектуре. Классы используются на 8 спринте. Там же можно будет применить, при желании, паттерны, но без сторонних библиотек.
+* Для сдачи необходимо переписать задание без применения модулей, стора, редьюсеров и пр. Стоит воспользоваться знаниями, которые даются в теоретической части к этому спринту.
+* 
+* Отлично: функционал работает корректно.*/
