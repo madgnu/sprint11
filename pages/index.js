3dd36aa90946976b5037791371fb6b4513167c1f
Васильев Михаил <madgnu@madg.nu>
2020-03-10 03:05:17 +0300

    lint

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const initialCards = [
-        { name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg' },
-        { name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg' },
-        { name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg' },
-        { name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg' },
-        { name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg' },
-        { name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg' },
-        { name: 'Нургуш', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg' },
-        { name: 'Тулиновка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg' },
-        { name: 'Остров Желтухина', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg' },
-        { name: 'Владивосток', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg' }
-    ];
+    { name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg' },
+    { name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg' },
+    { name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg' },
+    { name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg' },
+    { name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg' },
+    { name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg' },
+    { name: 'Нургуш', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg' },
+    { name: 'Тулиновка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg' },
+    { name: 'Остров Желтухина', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg' },
+    { name: 'Владивосток', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg' }
+];
 
 const cardList = document.querySelector('.places-list');
 const popup = document.querySelector('.popup');
@@ -32,14 +32,14 @@ const createCard = ({name, link}) => {
     el.classList = 'place-card';
     el.insertAdjacentHTML('beforeend', tpl);
     cardList.appendChild(el);
-}
+};
 
 const openForm = () => popup.classList.add('popup_is-opened');
 const closeForm = () => popup.classList.remove('popup_is-opened') || newForm.reset();
-const escapeForm = (event) => (popup.classList.contains('popup_is-opened') && event.key === 'Escape') ? closeForm(): null;
+const escapeForm = (event) => (popup.classList.contains('popup_is-opened') && event.key === 'Escape') ? closeForm() : null;
 const likeChange = (event) => (event.target.classList.contains('place-card__like-icon')) ? event.target.classList.toggle('place-card__like-icon_liked') : null;
 const deleteCard = (event) => (event.target.classList.contains('place-card__delete-icon')) ? event.target.parentNode.parentNode.remove() : null;
-const submitForm = (event) => { event.preventDefault(); createCard({ name: newForm.elements.name.value, link: newForm.elements.link.value }); closeForm(); }
+const submitForm = (event) => { event.preventDefault(); createCard({ name: newForm.elements.name.value, link: newForm.elements.link.value }); closeForm(); };
 
 cardList.addEventListener('click', likeChange);
 cardList.addEventListener('click', deleteCard);
